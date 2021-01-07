a26b69d5468b59d39debe1691b26e38204a05893
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2021-01-07 08:31:04 -0300

    Commit hover

--- crud/frontend/src/components/templates/Nav.jsx
+++ crud/frontend/src/components/templates/Nav.jsx
@@ -1,21 +1,20 @@
 import './Nav.css'
 import 'font-awesome/css/font-awesome.min.css'
 import React from 'react'
-
 export default props=>
 <aside className="menu-area">
     <nav className="menu">
         <a href="#/">
-            <i className="fa fa-home"></i>Inicio
+            <i className="fa fa-home"></i> Inicio
         </a>
         <a href="#/users">
-            <i className="fa fa-users"></i>Clientes
+            <i className="fa fa-users"></i> Clientes
         </a>
         <a href="#/servicos">
-        <i class="fas fa-tooth"></i>Serviços
+            <i class="fa fa-envelope-square"></i> Serviços
         </a>
         <a href="#/estoque">
-            <i class="fa fa-tooth"></i>Estoque
+            <i class="fas fa-database"></i> Estoque
         </a>
 
 
--- crud/frontend/src/main/Routes.jsx
+++ crud/frontend/src/main/Routes.jsx
@@ -1,14 +1,10 @@
-{/*Importando as dependencias
-O redirect funciona se você colocar a url que não tem nada a ver com nenhum dos componentes ele redireciona para a home*/}
-
 import React from 'react'
 import {Switch,Route,Redirect} from 'react-router'
-
 import Home from '../components/home/Home'
 import UserCrud from '../components/user/UserCrud'
 
 export default props=>
- <Switch> {/* Mapeamento entre a url e os componentes*/}
+ <Switch> 
      <Route exact path='/' component={Home} />
      <Route path ='/users' component={UserCrud} />
      <Redirect from='*' to='/' />
